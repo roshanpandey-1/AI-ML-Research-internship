db76d419c18bbd0fa46190059cf48bf738a41405
Nightfury1010 <roshanpandey72106@gmail.com>
2025-10-16 21:36:34 +0545

    iterators and generators

--- intermediatepython/advfunc.py
+++ intermediatepython/advfunc.py
@@ -115,91 +115,6 @@ def myfunction():
 
 print(myfunction.__name__)
 
-#generators 
-def my_generator():
-  yield 1
-  yield 2
-  yield 3
-
-for value in my_generator():
-  print(value)
-
-def count_up_to(n):
-  count = 1
-  while count <= n:
-    yield count
-    count += 1
-
-for num in count_up_to(5):
-  print(num)  
-
-def large_sequence(n):
-   for i in range(n):
-    yield i
-
-# This doesn't create a million numbers in memory
-gen = large_sequence(1000000)
-print(next(gen))
-print(next(gen))
-print(next(gen))
-
-def simple_gen():
-  yield "Emil"
-  yield "Tobias"
-  yield "Linus"
-
-gen = simple_gen()
-print(next(gen))
-print(next(gen))
-print(next(gen))
-
-# List comprehension - creates a list
-list_comp = [x * x for x in range(5)]
-print(list_comp)
-
-# Generator expression - creates a generator
-gen_exp = (x * x for x in range(5))
-print(gen_exp)
-print(list(gen_exp))
-
-# Calculate sum of squares without creating a list
-total = sum(x * x for x in range(10))
-print(total)
-
-def fibonacci():
-  a, b = 0, 1
-  while True:
-    yield a
-    a, b = b, a + b
-
-# Get first 100 Fibonacci numbers
-gen = fibonacci()
-for _ in range(10):
-  print(next(gen))
-
-def echo_generator():
-  while True:
-    received = yield
-    print("Received:", received)
-
-gen = echo_generator()
-next(gen) # Prime the generator
-gen.send("Hello")
-gen.send("World")
-
-
-def my_gen():
-  try:
-    yield 1
-    yield 2
-    yield 3
-  finally:
-    print("Generator closed")
-
-gen = my_gen()
-print(next(gen))
-gen.close()
-
 
 #recursion function
 def countdown(n):
@@ -256,3 +171,38 @@ print(find_max(my_list))
 import sys
 sys.setrecursionlimit(2000)
 print(sys.getrecursionlimit())
+
+#closure function
+def outer_function(msg):
+    def inner_function():
+        print("Message:", msg)  
+    return inner_function
+
+closure = outer_function("Hello Closure!")
+
+closure()  
+
+def counter():
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+    return increment
+
+c = counter()
+print(c())  
+print(c())  
+print(c())  
+
+
+def power(exponent):
+    def calc(base):
+        return base ** exponent
+    return calc
+
+square = power(2)
+cube = power(3)
+
+print(square(5))  
+print(cube(2))   
